cb2d3b7d77dd636d6c0b6f1dcd8a4936c9d580bd
haridhuang@outlook.com <hardihuang@outlook.com>
2018-04-20 21:49:41 +0800

    added shoulder left and right button for spinning left and right

--- development/nRF24L01-SimpleTransmit-01/nRF24L01-SimpleTransmit-01.ino
+++ development/nRF24L01-SimpleTransmit-01/nRF24L01-SimpleTransmit-01.ino
@@ -13,12 +13,14 @@
 /*-----( Declare Constants and Pin Numbers )-----*/
 #define JOYSTICK_X  A0
 #define JOYSTICK_Y  A1
+#define btnA A3 //shoulder left spinning
+#define btnB A2 //shoulder right spinning
 
-int btnA = 2; // right spinning
-int btnB = 3; //left spinning
-int btnC = 4; //joystick btn
-int btnD = 5; //undefined
-int btnE = 6; //undefined
+int btnC = 2; //undefined
+int btnD = 3; //undefined
+int btnE = 4; //undefined joystick btn
+int btnF = 5; //undefined
+int btnG = 6; //undefined
 
 /*-----( Declare objects )-----*/
 // (Create an instance of a radio, specifying the CE and CS pins. )
@@ -35,6 +37,8 @@ struct dataStruct {
   bool btnC;
   bool btnD;
   bool btnE;
+  bool btnF;
+  bool btnG;
 } myData;
 
 
@@ -46,6 +50,8 @@ void setup()
   pinMode(btnC, INPUT_PULLUP);
   pinMode(btnD, INPUT_PULLUP);
   pinMode(btnE, INPUT_PULLUP);
+  pinMode(btnF, INPUT_PULLUP);
+  pinMode(btnG, INPUT_PULLUP);
   myRadio.begin();
   myRadio.setChannel(108);
   myRadio.setDataRate(RF24_250KBPS); // Fast enough.. Better range
@@ -66,6 +72,8 @@ void loop()   /****** LOOP: RUNS CONSTANTLY ******/
   myData.btnC = !digitalRead(btnC);
   myData.btnD = !digitalRead(btnD);
   myData.btnE = !digitalRead(btnE);
+  myData.btnF = !digitalRead(btnF);
+  myData.btnG = !digitalRead(btnG);
   myData._micros = micros();
   /*
   Serial.print("X: ");
